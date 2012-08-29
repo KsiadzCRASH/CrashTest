43bb8ad83bd56846df49167a1b279a78272fb97d
KsiadzCRASH <ksiadzcrash@gmial.com>
2012-08-29 12:56:24 +0200

    Guava

---
+++ TestProject/src/main/collections/Sorting.java
@@ -0,0 +1,22 @@
+package main.collections;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class Sorting extends TestCase{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Test
+	public void sortTest()
+	{
+		
+	}
+}
